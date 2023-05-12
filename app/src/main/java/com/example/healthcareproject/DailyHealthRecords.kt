43d53a5e64d3303721583a4d390005926af2cdce
OVKJCT <tkdduq333@naver.com>
2023-05-13 01:25:15 +0900

    Adjust button position in main activity and Switch Activity

---
+++ app/src/main/java/com/example/healthcareproject/DailyHealthRecords.kt
@@ -0,0 +1,11 @@
+package com.example.healthcareproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DailyHealthRecords : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daily_health_records)
+    }
+}
--- app/src/main/java/com/example/healthcareproject/MainActivity.kt
+++ app/src/main/java/com/example/healthcareproject/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.healthcareproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.healthcareproject.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        //바인딩 객체 획득
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        //액티비티 화면 출력
+        setContentView(binding.root)
+
+        val intent1 = Intent(this, Profile::class.java)
+        binding.ProfileButton.setOnClickListener {startActivity(intent1)}
+
+        val intent2 = Intent(this, DailyHealthRecords::class.java)
+        binding.DailyHealthRecordsButton.setOnClickListener {startActivity(intent2)}
+
+        val intent3 = Intent(this, Calender::class.java)
+        binding.CalenderButton.setOnClickListener {startActivity(intent3)}
+
+        val intent4 = Intent(this, BodyCheck::class.java)
+        binding.BodyCheckButton.setOnClickListener {startActivity(intent4)}
+
+        val intent5 = Intent(this, Recommendation::class.java)
+        binding.RecommendationButton.setOnClickListener {startActivity(intent5)}
+
     }
 }
