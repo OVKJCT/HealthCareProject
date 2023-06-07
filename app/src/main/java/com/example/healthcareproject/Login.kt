abd48038349f3168f17a6d66db46c2c1a3080121
OVKJCT <tkdduq333@naver.com>
2023-06-07 10:57:37 +0900

    Login and Join Membership screen(2)

--- app/src/main/java/com/example/healthcareproject/Login.kt
+++ app/src/main/java/com/example/healthcareproject/Login.kt
@@ -3,12 +3,21 @@ package com.example.healthcareproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.healthcareproject.databinding.ActivityLoginBinding
 import com.example.healthcareproject.databinding.ActivityRecommendationBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+private lateinit var auth : FirebaseAuth
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        auth = Firebase.auth
+
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_login)
 
         val binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -16,7 +25,29 @@ class Login : AppCompatActivity() {
         setContentView(binding.root)
 
         val intent14 = Intent(this, Membership::class.java)
+
         binding.MembershipButton1.setOnClickListener { startActivity(intent14) }
+
+        val intent16 = Intent(this, MainActivity::class.java)
+
+        binding.LoginButton.setOnClickListener {
+            val email: String = binding.EmailAddressText1.text.toString()
+            val password: String = binding.PasswordText1.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    binding.EmailAddressText1.text.clear()
+                    binding.PasswordText1.text.clear()
+
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "로그인에 성공했습니다!", Toast.LENGTH_SHORT).show()
+                        startActivity(intent16)
+                    }
+                    else {
+                        Toast.makeText(this, "아이디와 비밀번호를 정확하게 입력해주세요.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
 
     }
 }
--- app/src/main/java/com/example/healthcareproject/Membership.kt
+++ app/src/main/java/com/example/healthcareproject/Membership.kt
@@ -3,12 +3,21 @@ package com.example.healthcareproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.healthcareproject.databinding.ActivityLoginBinding
 import com.example.healthcareproject.databinding.ActivityMembershipBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+private lateinit var auth : FirebaseAuth
 
 class Membership : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        auth = Firebase.auth
+
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_membership)
 
         val binding = ActivityMembershipBinding.inflate(layoutInflater)
@@ -16,8 +25,27 @@ class Membership : AppCompatActivity() {
         setContentView(binding.root)
 
         val intent15 = Intent(this, Login::class.java)
+
         binding.LoginBackButton.setOnClickListener { startActivity(intent15) }
 
+        binding.MembershipButton2.setOnClickListener {
+            val email: String = binding.EmailAddressText2.text.toString()
+            val password: String = binding.PasswordText2.text.toString()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    binding.EmailAddressText2.text.clear()
+                    binding.PasswordText2.text.clear()
+
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "회원가입에 성공했습니다!", Toast.LENGTH_SHORT).show()
+                        startActivity(intent15)
+                    }
+                    else {
+                        Toast.makeText(this, "회원가입에 실패하였습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
 
     }
 }
