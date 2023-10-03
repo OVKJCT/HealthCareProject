657be980fddffb59ff5f4522035bf446abc56305
OVKJCT <tkdduq333@naver.com>
2023-10-03 19:30:55 +0900

    Login and Join Membership screen(2)

--- app/src/main/java/com/example/healthcareproject/BodyCheck.kt
+++ app/src/main/java/com/example/healthcareproject/BodyCheck.kt
@@ -20,6 +20,19 @@ class BodyCheck : AppCompatActivity() {
         val intent10 = Intent(this, MainActivity::class.java)
         binding.HomeButton4.setOnClickListener {startActivity(intent10)}
 
+        val schedules = ArrayList<String>()
+
+        schedules.add("일정 1")
+        schedules.add("일정 2")
+        schedules.add("일정 3")
+        schedules.add("일정 4")
+        schedules.add("일정 5")
+        schedules.add("일정 6")
+        schedules.add("일정 7")
+        schedules.add("일정 8")
+
+        val schedules_Adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, schedules)
+        binding.listSchedules.adapter = schedules_Adapter
 
     }
 }
