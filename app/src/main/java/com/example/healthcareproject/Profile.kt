bbe8a31c1248d64ca4ad3316650cc87a9de809df
OVKJCT <tkdduq333@naver.com>
2023-10-16 06:32:32 +0900

    Login and Join Membership screen(2)

--- app/src/main/java/com/example/healthcareproject/Profile.kt
+++ app/src/main/java/com/example/healthcareproject/Profile.kt
@@ -3,11 +3,23 @@ package com.example.healthcareproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.example.healthcareproject.databinding.ActivityMainBinding
 import com.example.healthcareproject.databinding.ActivityProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserInfo
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+
 
 class Profile : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
@@ -16,6 +28,13 @@ class Profile : AppCompatActivity() {
         setContentView(binding.root)
 
         val intent8 = Intent(this, MainActivity::class.java)
-        binding.HomeButton1.setOnClickListener {startActivity(intent8)}
+        binding.HomeButton1.setOnClickListener { startActivity(intent8) }
+
+        val database = Firebase.database
+
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Success!!!")
+
     }
 }
